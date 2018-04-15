0c9d8fee2c253b4bb6b859262d05fde0badd38ff
Kwaku Dapaah <kwakudapaah@Kwakus-MBP.fios-router.home>
2018-04-15 17:02:59 -0400

    changes

--- djorg/urls.py
+++ djorg/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.views.generic import TemplateView
+from django.urls import include, path
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='base.html')),
+    path('bookmarks/', include('bookmarks.urls')),
     path('admin/', admin.site.urls),
 ]
